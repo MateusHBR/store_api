9c9b29b5757508d30250d8a10a3d1937a5dc0c12
MateusHBR <mateusmorais.r@gmail.com>
2022-02-09 18:09:25 -0300

    fix up and down migrations

--- adapter/database/migration/000001_create_product_table.down.sql
+++ adapter/database/migration/000001_create_product_table.down.sql
@@ -1,8 +1,1 @@
-CREATE TABLE IF NOT EXISTS product(
-  id VARCHAR(255) NOT NULL PRIMARY KEY,
-  name VARCHAR(100) NOT NULL,
-  description TEXT,
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP,
-  removed_at TIMESTAMP
-);
+DROP TABLE IF EXISTS products;
--- adapter/database/migration/000001_create_product_table.up.sql
+++ adapter/database/migration/000001_create_product_table.up.sql
@@ -1,1 +1,9 @@
-DROP TABLE IF EXISTS product;
+
+CREATE TABLE IF NOT EXISTS products(
+  id VARCHAR(255) NOT NULL PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  description TEXT,
+  created_at TIMESTAMP,
+  updated_at TIMESTAMP,
+  removed_at TIMESTAMP
+);
