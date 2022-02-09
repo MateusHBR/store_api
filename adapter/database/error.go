7e5255edebe894761c4b2a515c8b551c01a46e0a
MateusHBR <mateusmorais.r@gmail.com>
2022-02-08 23:49:22 -0300

    update no rows errors

--- adapter/database/error.go
+++ adapter/database/error.go
@@ -5,6 +5,13 @@ import (
 	"errors"
 )
 
+// IsNoRowError identifies a row error type
+// It allow both directly identification and unwrap one
+// for those who wraps error using fmt.Errorf("... %w ...")
 func IsNoRowsError(err error) bool {
-	return errors.Is(err, sql.ErrNoRows)
+	if ok := errors.Is(err, sql.ErrNoRows); ok {
+		return ok
+	}
+
+	return errors.Is(errors.Unwrap(err), sql.ErrNoRows)
 }
